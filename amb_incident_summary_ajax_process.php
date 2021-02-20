bd088d8d36ed53ea2d0895a2914d9f49795bf0d2
varshasraut <varsha.speroinfosystems@gmail.com>
2021-02-20 11:03:43 +0530

    listing amb
    listing amb

--- amb_incident_summary_ajax_process.php
+++ amb_incident_summary_ajax_process.php
@@ -45,14 +45,21 @@ if($_REQUEST['action']=='view_amb_list')
             </thead>
             <tbody>
             <?php 
+$sql ="SELECT amb.*,amb.id as amb_id,amb_ty.id as ty_id,amb_st.id as st_id,amb_base.base_name as bs_nm,amb_ty.amb_type,amb_st.amb_status " 
+. "$radius FROM sp_ems_ambulance as amb"
+. " LEFT JOIN sp_ems_amb_status as amb_st ON amb_st.id = amb.amb_status"
+. " LEFT JOIN sp_ems_amb_type as amb_ty  ON amb_ty.id = amb.amb_type"
+. " LEFT JOIN sp_ems_base_location as amb_base ON amb_base.id = amb.base_loc"
+. " WHERE 1=1 "
+. " GROUP BY amb.amb_no $orderby LIMIT 5";
 
-$sql ="SELECT amb.*,amb.id as amb_id,amb_ty.id as ty_id,amb_st.id as st_id,amb_base.base_name as bs_nm,amb_ty.amb_type,amb_st.amb_status 
+/*$sql ="SELECT amb.*,amb.id as amb_id,amb_ty.id as ty_id,amb_st.id as st_id,amb_base.base_name as bs_nm,amb_ty.amb_type,amb_st.amb_status 
 FROM sp_ems_ambulance as amb 
 LEFT JOIN sp_ems_amb_status as amb_st ON amb_st.id = amb.amb_status
 LEFT JOIN sp_ems_amb_type as amb_ty  ON amb_ty.id = amb.amb_type
 LEFT JOIN sp_ems_base_location as amb_base ON amb_base.id = amb.base_loc
 WHERE 1=1 
-GROUP BY amb.amb_no $orderby LIMIT 5";
+GROUP BY amb.amb_no $orderby LIMIT 5";*/
 
 $AllRrecord = $db->fetch_all_array($sql);
 foreach($AllRrecord as $key=>$valRecords)
